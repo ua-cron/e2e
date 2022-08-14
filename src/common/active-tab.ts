d528bddf319548a8bc1dddaa53ac4d742e4165a9
SERHIIBZENKD8F4\serhiibzenko <bzenkosergey@gmail.com>
2022-08-14 20:07:54 +0200

    initial

--- src/common/active-tab.ts
+++ src/common/active-tab.ts
@@ -4,32 +4,26 @@ import { CronType } from './../enums/cron-type.enum';
 import { getSharedHelper } from './helpers';
 
 export const testActiveTab = (type: string) => {
-	const { findInContainer, getCronType } = getSharedHelper(type);
-	const tabs = getCronType() === CronType.QUARTZ ? [
-		Type.DAY,
-		Type.HOURS,
-		Type.MINUTES,
-		Type.MONTH,
-		Type.SECONDS,
-		Type.YEAR
-	] : [
-		Type.DAY,
-		Type.HOURS,
-		Type.MINUTES,
-		Type.MONTH
-	];
+  const { findInContainer, getCronType } = getSharedHelper(type);
+  const tabs = getCronType() === CronType.QUARTZ ? [
+    Type.DAY,
+    Type.HOURS,
+    Type.MINUTES,
+    Type.MONTH,
+    Type.SECONDS,
+    Type.YEAR
+  ] : [
+    Type.DAY,
+    Type.HOURS,
+    Type.MINUTES,
+    Type.MONTH
+  ];
 
-	describe(`Active tab should work with ${type}`, () => {
-		beforeEach(() => {
-			cy.visit('/');
-		});
-
-		tabs.forEach(t => {
-			it(`state should be disabled ${t}`, () => {
-				findInContainer(`[data-cron-action="change-tab"][data-cron-action-value="${t}"]`).click();
-				findInContainer(`.c-tab.${t}`).should('have.class', 'active');
-				findInContainer(`.c-tab-content[tab-name="${t}"]`).should('have.length', 1);
-			});
-		});
-	});
+  describe(`should work with ${type}`, () => {
+    tabs.forEach(t => it(`state should be disabled ${t}`, () => {
+      findInContainer(`[data-cron-action="change-tab"][data-cron-action-value="${t}"]`).click();
+      findInContainer(`.c-tab.${t}`).should('have.class', 'active');
+      findInContainer(`.c-tab-content[tab-name="${t}"]`).should('have.length', 1);
+    }));
+  });
 };
--- src/specs/active-tab.spec.ts
+++ src/specs/active-tab.spec.ts
@@ -1,7 +1,7 @@
 import { testActiveTab } from './../common/active-tab';
 
 export const activeTab = () => describe('Active Tab', () => {
-    testActiveTab('ReCron');
-    testActiveTab('ReUnixCron');
-    testActiveTab('ReQuartzCron');
+  testActiveTab('ReCron');
+  testActiveTab('ReUnixCron');
+  testActiveTab('ReQuartzCron');
 });
